0c10c7767eb360f29139aa9477117873afb806e2
Alejandrasalamanca <masalamanca96@misena.edu.co>
2016-03-09 13:43:56 -0500

    Divicion del trabajo

---
+++ ServicioBD/App_Code/ClConexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de ClConexion
+/// </summary>
+public class ClConexion
+{
+	public ClConexion()
+	{
+        
+	}
+}
