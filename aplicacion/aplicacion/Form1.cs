77eca0d8f962ffb05d657090b70772b060bbd1fd
Alejandrasalamanca <masalamanca@misena.edu.co>
2016-03-09 13:10:38 -0500

    Integracion

---
+++ aplicacion/aplicacion/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace aplicacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.ServiceSoapClient m = new ServiceReference1.ServiceSoapClient();
+            int valor1 = Convert.ToInt32(textBox1.Text);
+            int valor2 = Convert.ToInt32(textBox2.Text);
+
+
+            int resultado = m.Calcular(valor1, valor2);
+            label1.Text = "El resultado es: " + resultado; 
+        }
+    }
+}
